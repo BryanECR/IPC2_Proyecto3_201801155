ae030265e4eba86900e193e7589ffe8b05b64252
BryanECR <70345203+BryanECR@users.noreply.github.com>
2021-05-03 14:45:14 -0500

    3/05/2021
    template, frontend

---
+++ Backend/Lista/Lista.py
@@ -0,0 +1,56 @@
+from Lista.Nodo import Nodo
+
+class ListaEnlazada:
+    def __init__(self):
+        self.primero = None
+        self.ultimo = None
+
+    #CONVIERTE UNA TUPLA EN UN STRING
+    def convetTuple(tup):
+        cadena = ''.join(tup)
+        return cadena
+
+    def vacia(self):
+        return self.primero == None
+
+    def incertar(self,fecha,usuario,afectado,codigo,error):
+        if self.vacia():
+            self.primero = self.ultimo = Nodo(fecha,usuario,afectado,codigo,error)
+        else:
+            aux = self.ultimo
+            self.ultimo = aux.siguiente = Nodo(fecha,usuario,afectado,codigo,error)
+
+    def retornarInfomacion(self):
+        aux = self.primero
+        datos = []
+        while(aux != None):
+            fecha = ListaEnlazada.convetTuple(aux.fecha)
+            usuario = ListaEnlazada.convetTuple(aux.usuario)
+            afectado = ListaEnlazada.convetTuple(aux.afectado)
+            codigo = ListaEnlazada.convetTuple(aux.codigo)
+            error = ListaEnlazada.convetTuple(aux.error)
+            informacion = {"fecha":fecha,"usuario":usuario,"afectado":afectado,"codigo":codigo,"error":error}
+            #print("fecha"+fecha,"usuario"+usuario,"afectado"+afectado,"codigo"+codigo,"error"+error)
+            datos.append(informacion)
+
+            aux = aux.siguiente
+        
+        return datos
+
+    def busqueda(self,dato1,dato2):
+        aux = self.primero
+        datos = []
+        while(aux != None):
+            fecha = ListaEnlazada.convetTuple(aux.fecha)
+            usuario = ListaEnlazada.convetTuple(aux.usuario)
+            afectado = ListaEnlazada.convetTuple(aux.afectado)
+            codigo = ListaEnlazada.convetTuple(aux.codigo)
+            error = ListaEnlazada.convetTuple(aux.error)
+
+            if(fecha == dato1 and usuario == dato2) or (fecha == dato1 and codigo == dato2):
+                informacion = {"fecha":fecha,"usuario":usuario,"afectado":afectado,"codigo":codigo,"error":error}
+                datos.append(informacion)
+
+            aux = aux.siguiente
+        
+        return datos
--- P3_Backend_IPC2/Lista/Nodo.py
+++
@@ -1,8 +0,0 @@
-class Nodo:
-    def __init__(self,fecha,usuario,afectado,codigo,error):
-        self.fecha = fecha,
-        self.usuario = usuario,
-        self.afectado = afectado,
-        self.codigo = codigo,
-        self.error = error,
-        self.siguiente = None
